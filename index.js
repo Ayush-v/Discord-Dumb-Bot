b35f22f435cb43699b686b68ecbefaea2b862500
Ayush <62958067+Ayush-v@users.noreply.github.com>
2022-09-02 15:28:36 +0530

    my first bot

---
+++ index.js
@@ -0,0 +1,37 @@
+const dotenv = require("dotenv");
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
+
+dotenv.config();
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply(
+      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
+    );
+  } else if (commandName === "user") {
+    await interaction.reply(
+      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
+    );
+  } else if (commandName === "create") {
+    await interaction.reply("yes its working you did it");
+  }
+});
+
+// Login to Discord with your client's token
+client.login(token);
